27bdc42bd56803b619abc3f18a7794f9830d21d3
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-26 11:40:35 +0200

    Ajoutez des fichiers projet.

---
+++ I3.WAD21.Exos.Ado01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace I3.WAD21.Exos.Ado01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONN_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True";
+
+            //SqlConnection connection = new SqlConnection();
+            //connection.ConnectionString = CONN_STRING;
+
+            SqlConnection connection = new SqlConnection(CONN_STRING);
+
+            Console.WriteLine(connection.State);
+            connection.Open();
+            Console.WriteLine(connection.State);
+            connection.Close();
+            Console.WriteLine(connection.State);
+        }
+    }
+}
---
+++ I3.WAD21.Exos.Ado02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace I3.WAD21.Exos.Ado02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONN_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(CONN_STRING))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText =
+                        "SELECT student_id, first_name, last_name FROM student";
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                            Console.WriteLine($"{reader["student_id"]} {reader["first_name"]} {reader["last_name"]}");
+                    }
+                }
+
+                connection.Close();
+
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT AVG(year_result) FROM student";
+                    connection.Open();
+                    int moyenne = (int)command.ExecuteScalar();
+                    Console.WriteLine($"La moyenne de la classe est de {moyenne} sur 20.");
+                }
+            }
+        }
+    }
+}
