b0eef3a2e53b7072aa865a25ad2b51ad9896624f
Legrain Samuel <samuel.legrain@bstorm.be>
2021-11-08 13:31:03 +0100

    Update ADO03

--- I3.WAD21.Exos.Ado03/Program.cs
+++ I3.WAD21.Exos.Ado03/Program.cs
@@ -18,13 +18,27 @@ namespace I3.WAD21.Exos.Ado03
             me.login = "slegrain";
 
             const string CONN_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True";
-
             using (SqlConnection connection = new SqlConnection(CONN_STRING))
             {
                 using (SqlCommand command = connection.CreateCommand())
                 {
+                    SqlParameter p_firstname = new SqlParameter { ParameterName = "fn", Value = me.first_name };
+                    command.Parameters.Add(p_firstname);
+                    SqlParameter p_lastname = new SqlParameter { ParameterName = "ln", Value = me.last_name };
+                    command.Parameters.Add(p_lastname);
+                    //command.Parameters.AddWithValue("ln", me.last_name);
+                    SqlParameter p_login = new SqlParameter { ParameterName = "lg", Value = me.login };
+                    command.Parameters.Add(p_login);
+                    SqlParameter p_birthdate = new SqlParameter { ParameterName = "bd", Value = me.birth_date };
+                    command.Parameters.Add(p_birthdate);
+                    SqlParameter p_sectionid = new SqlParameter { ParameterName = "sid", Value = me.section_id };
+                    command.Parameters.Add(p_sectionid);
+                    SqlParameter p_courseid = new SqlParameter { ParameterName = "cid", Value = me.course_id };
+                    command.Parameters.Add(p_courseid);
+                    SqlParameter p_yearresult = new SqlParameter { ParameterName = "yr", Value = me.year_result };
+                    command.Parameters.Add(p_yearresult);
                     command.CommandText = 
-                        $"INSERT INTO student (first_name, last_name, year_result, login, course_id, section_id, birth_date) OUTPUT inserted.student_id VALUES ('{me.first_name}', '{me.last_name}', {me.year_result}, '{me.login}', '{me.course_id}', {me.section_id}, '{me.birth_date.ToString("yyyy-MM-dd")}')";
+                        $"INSERT INTO student (first_name, last_name, year_result, login, course_id, section_id, birth_date) OUTPUT inserted.student_id VALUES (@fn, @ln, @yr, @lg, @cid,@sid, @bd)";
                     connection.Open();
                     me.student_id = (int)command.ExecuteScalar();
                 }
